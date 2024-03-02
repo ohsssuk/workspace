af6f7b10602111c200cec9323e2034f379eabd0a
ohsssuk <ohsssuk@naver.com>
2024-03-03 03:58:38 +0900

    저장

---
+++ dho-v1/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+import './reset.css';
+
+const SITE_TITLE = '대항오 계산기';
+const SITE_DESCRIPTION = '대항오 계산기';
+
+export const metadata: Metadata = {
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  keywords: '대항해시대, 대항해시대 오리진, 게임, 계산기, 편의기능',
+  openGraph: {
+    type: 'website',
+    url: 'http://www.lifebefore.co.kr/dho-cal',
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    siteName: SITE_TITLE,
+    images: [
+      {
+        url: 'http://www.lifebefore.co.kr/images/thumb.jpg',
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
--- dho/src/constants/colors.tsx
+++
@@ -1,9 +0,0 @@
-const colors = {
-  keyColor: '#46433f',
-  activeColor: '#46433f',
-  errorColor: '#46433f',
-
-  bonusValue: '#085512',
-};
-
-export default colors;
